f2fb2e1e3ff2da949d49cbfc0b16f700eb31c844
Silak <silak@mango-byte.com>
2022-08-12 11:12:47 +0700

    chore: :technologist: improve index

--- web/app/themes/cam-theme/resources/views/index.blade.php
+++ web/app/themes/cam-theme/resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   @include('partials.page-header')
-  <h1 class="hello">Hello php</h1>
+  <h1 class="text-red-600 text-2xl">Hello from red color</h1>
   @if (! have_posts())
     <x-alert type="warning">
       {!! __('Sorry, no results were found.', 'sage') !!}
